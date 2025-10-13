f10cbd2f1bc080fe5307382cd3ce61d8f8347671
Juan Ramon <gekkoone7@gmail.com>
2025-10-12 23:33:05 -0600

    Añadiendo dinamismo a las imagenes con v-bind

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -11,11 +11,14 @@
 <template>
      <div class="col-md-6 col-lg-4 my-4 row align-items-center">
                 <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_01.jpg" alt="imagen guitarra">
+                    <img class="img-fluid" 
+                    v-bind:src=" '/img/' + guitarra.imagen + '.jpg' " 
+                    v-bind:alt="'imagen guitarra' + guitarra.nombre"
+                    >
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>{{ guitarra.descripcion }}</p>
                     <p class="fw-black text-primary fs-3">$299</p>
                     <button 
                         type="button"
